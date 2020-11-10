65402bbac9c257990e8c4fa7a5367506b12f80d4
lucia8716 <crespopopo@gmail.com>
2020-11-10 13:02:46 +0100

    v1

--- PHP/acertijo/elegir.php
+++ PHP/acertijo/elegir.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
 </head>
 
 <body>
@@ -12,11 +12,11 @@
 
 
 
-    <?php
+        <?php
 
-    if (!$_POST) {
+        if (!$_POST) {
 
-        echo '<form method="POST" action="">
+                echo '<form method="POST" action="">
 
 <h1>Introduce cuantro digitos</h1></br></br></br>
 <input type="number" name="n1">&nbsp;&nbsp;<input type="number" name="n2">
@@ -24,35 +24,35 @@
 <input type="submit" name="enviar">
 </form></br></br></br>';
 
-        $n1 = rand(1, 10);
-        $n2 = rand(1, 10);
-        $n3 = rand(1, 10);
-        $n4 = rand(1, 10);
+                $n1 = rand(1, 10);
+                $n2 = rand(1, 10);
+                $n3 = rand(1, 10);
+                $n4 = rand(1, 10);
 
-        echo "la solucion al ejercicio es " . $n1 . $n2 . $n3 . $n4;
-    } else {
+                echo "la solucion al ejercicio es " . $n1 . $n2 . $n3 . $n4;
+        } else {
 
-        $numero1 = $_POST['n1'];
-        $numero2 = $_POST['n2'];
-        $numero3 = $_POST['n3'];
-        $numero4 = $_POST['n4'];
+                $numero1 = $_POST['n1'];
+                $numero2 = $_POST['n2'];
+                $numero3 = $_POST['n3'];
+                $numero4 = $_POST['n4'];
 
-        echo "<form method='POST' action=''>
+                echo "<form method='POST' action=''>
 
 <h1>Introduce cuantro digitos</h1></br></br></br>
 <input type='number' name='n1' value='$numero1'>";
-        echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero2'>
+                echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero2'>
 &nbsp;&nbsp;";
-        echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero3'>
+                echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero3'>
 &nbsp;&nbsp;";
-        echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero4'>
+                echo "&nbsp;&nbsp;<input type='number' name='n2' value='$numero4'>
 &nbsp;&nbsp;";
-    }
+        }
 
 
 
 
-    ?>
+        ?>
 
 
 
