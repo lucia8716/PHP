5fa898547d08ad651646544a4b38f15ae2e40b05
lucia8716 <crespopopo@gmail.com>
2020-11-24 08:46:47 +0100

    v1

---
+++ PHP/tema4/2/paginabuena.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$pizzas=["Jamon y queso","Napolitana","Mozarella"];
+
+echo' <form method="post" action="1.php">
+
+Nombre:
+<input type="text" name "nombre" size="30">
+<br>
+
+Direccion;
+<input type="text" name="direccion" size="40">
+<br>';
+
+for ($x=0; $x < 3; $x++) { 
+    echo $pizzas[$x];
+}
+
+echo "<input type='checkbox' name='pizza[]' value=$pizzas[$x]";
+echo "<input type='checkbox' name='cantidad[]' size='3' <br>";
+echo '<input type="submit" value="Confirmar"</form>'
+
+
+?>
+
+
+</body>
+</html>
